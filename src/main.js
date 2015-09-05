6d97d52190cd3667ee02df6e82aee47ed32f086d
aroskar <aroskar@yahoo-inc.com>
2015-09-04 19:56:46 -0700

    first commit for simple vanilla promise implementation

---
+++ src/main.js
@@ -0,0 +1,42 @@
+module.exports = {
+    defer: function () {
+        var resolvedCallback,
+            rejectedCallback,
+            data,
+            error,
+            actionCompleted = false;
+
+        return {
+            promise: {
+                then: function (success, fail) {
+                    resolvedCallback = success;
+
+                    if (actionCompleted  && data) {
+                        resolvedCallback.call(null, data);
+                    }
+
+                    if (actionCompleted && error) {
+                        rejectedCallback.call(null, error);
+                    }
+
+                }
+            }, 
+            resolve: function (d) {
+                data = d;
+                actionCompleted = true;
+
+                if (resolvedCallback) {
+                    resolvedCallback.call(null, d);
+                }
+            },
+            reject: function (e) {
+                error = e;
+                actionCompleted = true;
+
+                if (rejectedCallback) {
+                    rejectedCallback.call(null, d);
+                }
+            }
+        }
+    }
+};
